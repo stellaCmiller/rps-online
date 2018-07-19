9213827b14dc228af55c7b108cc0424ceddf4436
Stella Miller <38590261+stellaCmiller@users.noreply.github.com>
2018-07-18 21:48:41 -0500

    bug fixing 2, what even is git amend

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -89,9 +89,11 @@ var rockPaperScissors = {
 
     /*When the player makes a choice, firebase is updated with their selection */
     logSelection: function(choice){
-        db.ref(`/Queue/${this.player.queueKey}`).update({
-            selection: choice
-        });
+        console.log(this.player.queueKey);
+        console.log(choice);
+        //db.ref(`/Queue/${this.player.queueKey}`).update({
+           // selection: choice
+        //});
         //this.checkSelections();
     },
 
